94882e5957e5570abb6284096b20d1ece829812d
mahyab <mahyab79@gmail.com>
2024-02-25 23:21:21 +0330

    set up submit form  for create new Isuue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,16 +2,36 @@
 import { Flex, TextArea, TextField, Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-
+import axios from "axios";
+import { Controller, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+interface IIssueForm {
+  title: string;
+  description: string;
+}
 const NewIssuePage = () => {
+    const { register, handleSubmit, control } = useForm<IIssueForm>();
+    const router = useRouter();
   return (
-    <div className="max-w-xl space-y-3 ">
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues")
+      })}
+      className="max-w-xl space-y-3 "
+    >
       <Flex direction="column" gap="3">
-        <TextField.Input placeholder="Title" />
-        <SimpleMDE placeholder="description" />
+        <TextField.Input {...register("title")} placeholder="Title" />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="description" {...field} />
+          )}
+        />
       </Flex>
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 export default NewIssuePage;
