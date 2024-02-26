86adedd9d4fa0173fc91f33fe2854eb28e013ee6
mahyab <mahyab79@gmail.com>
2024-02-27 00:12:29 +0330

    format zod custom description error

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,20 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import {z} from 'zod';
-import prisma from "@/prisma/client"
+import { z } from "zod";
+import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
-    title:z.string().min(1).max(255),
-    description: z.string().min(1).max(255)
-})
-export async function POST(request:NextRequest) {
-    const body = await request.json();
-    const validation = createIssueSchema.safeParse(body);
-    if(!validation.success) 
-       return NextResponse.json(validation.error.errors,{status:400})
-    
-    const newsIssue = await prisma.issue.create({
-        data:{title:body.title,description:body.description  }
-    })
-    return NextResponse.json(newsIssue , {status:201})
- 
+  title: z.string().min(1).max(255),
+  description: z.string().min(1 , 'the description is required').max(255),
+});
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const newsIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+  return NextResponse.json(newsIssue, { status: 201 });
 }
